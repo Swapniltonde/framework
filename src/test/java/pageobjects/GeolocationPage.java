0130565209ed910a74ac51d8d0f77b3ec2c070a2
swapniltonde <swapniltonde55@gmail.com>
2018-06-12 21:32:53 +0530

    added geolocation class

--- src/test/java/pageobjects/GeolocationPage.java
+++ src/test/java/pageobjects/GeolocationPage.java
@@ -2,4 +2,9 @@ package pageobjects;
 
 public class GeolocationPage {
 
+	
+	String xpathHeader = "//h3";
+	String idButtondDescription = "demo";
+	//String xpathbutton = "//button[@onclick='getLocation()']";
+	String xpathbButton = "//button[contains(text(),'Where am I?')]";
 }
