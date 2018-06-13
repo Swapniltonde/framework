c4726500226b8870cfc0d023dd78be167fd01556
Swapnil Tonde <swapnil.tonde@nihilent.com>
2018-06-13 16:11:04 +0530

    added 3 new class for locaters

---
+++ src/test/java/pageobjects/ContextMenuPage.java
@@ -0,0 +1,12 @@
+package pageobjects;
+
+public class ContextMenuPage {
+	
+	String XpathHeader="//h3";
+	String XpathMenuItemsText="//p[contains(text(),'Context menu items are custom additions that appear in the right-click menu.')]";
+	String XpathBoxText="/html/body/div[2]/div//div[@class='example']/p[2]";
+	String idSquareBox="hot-spot";
+	String XpathNoteText="//strong";
+	String Linktext=" this open issue";
+
+}
---
+++ src/test/java/pageobjects/DynamicLoadingPage.java
@@ -0,0 +1,10 @@
+package pageobjects;
+
+public class DynamicLoadingPage {
+	
+	String LinkText1 = "Example 1: Element on page that is hidden";
+	String LinkText2 = "Example 2: Element rendered after the fact";
+	String XpathHeader = "//h3";
+	String XpathTest1="/html/body/div[2]//div[@class='example']/p[1]";
+	String XpathTest2="/html/body/div[2]//div[@class='example']/p[2]";
+}
