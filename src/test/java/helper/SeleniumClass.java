21de43c3712e2a415ee4db86a3700dd537a84901
Veta123 <veta.v.gawas@gmail.com>
2018-06-13 20:51:16 +0530

    added method launchApp in SeleniumClass

--- src/test/java/helper/SeleniumClass.java
+++ src/test/java/helper/SeleniumClass.java
@@ -3,13 +3,34 @@ package helper;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class SeleniumClass extends Base{
 	
 	WebDriver driver;
 	
-	public void launchApplication(String browserName, String url) {
-		
+	public void launchApplication(String browserName, String url) 
+	{
+		if(browserName.equalsIgnoreCase("Chrome Browser")) 
+		{
+			driver = new ChromeDriver();
+			
+		}
+		else if(browserName.equalsIgnoreCase("Firefox Browser")) 
+		{
+			driver = new FirefoxDriver();
+		}
+		else if(browserName.equalsIgnoreCase("IE Browser")) 
+		{
+			driver = new InternetExplorerDriver();
+		}
+		else 
+		{
+			System.out.println("Invalid browser name");
+		}
 		
 	}
 	
--- src/test/java/pageobjects/DynamicContentPage.java
+++ src/test/java/pageobjects/DynamicContentPage.java
@@ -13,12 +13,12 @@ public class DynamicContentPage
 	//String xpathLinkTextClickHere = "//a[@href='/dynamic_content?with_content=static']";
 	
 	String xpathImage1 = "//img[@src='/img/avatars/Original-Facebook-Geek-Profile-Avatar-2.jpg']";
-	String xpathParagraph3 = "//div[@class = 'large-10 columns']";
+	String xpathParagraph3 = "//div[@class = 'row'][1]/div[@class = 'large-10 columns']";
 	
 	String xpathImage2 = "//img[@src='/img/avatars/Original-Facebook-Geek-Profile-Avatar-2.jpg']";
-	String xpathParagraph4 = "//div[@class = 'large-10 columns']";
+	String xpathParagraph4 = "//div[@class = 'row'][2]/div[@class = 'large-10 columns']";
 	
 	String xpathImage3 = "//img[@src='/img/avatars/Original-Facebook-Geek-Profile-Avatar-2.jpg']";
-	String xpathParagraph5 = "//div[@class = 'large-10 columns']";
+	String xpathParagraph5 = "//div[@class = 'row'][3]/div[@class = 'large-10 columns']";
 	//String xpathParagraph5 = "//div[contains(@class, 'large-10 columns')]";
 }
