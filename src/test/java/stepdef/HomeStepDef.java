31d4f5248210419bd73fb80c5c4274860b3fa451
chaitali90 <chaitali.gaikwad90@gmail.com>
2018-06-22 20:02:11 +0530

    chaitali's changes

--- src/test/java/stepdef/HomeStepDef.java
+++ src/test/java/stepdef/HomeStepDef.java
@@ -1,5 +1,36 @@
 package stepdef;
 
-public class HomeStepDef {
 
+import cucumber.api.java.en.*;
+import pageobjects.HomePage;
+
+public class HomeStepDef extends HomePage{
+
+	@Given("^User has launched \"([^\"]*)\" browser with url \"([^\"]*)\"$")
+	public void user_has_launched_browser_with_url(String arg1, String arg2) throws Throwable {
+	    
+		System.out.println("In given");
+		this.launchApplication(arg1, arg2);
+	    
+	}
+
+	@Given("^verify page by \"([^\"]*)\" with \"([^\"]*)\"$")
+	public void verify_page_by_with(String arg1, String arg2) throws Throwable {
+	  
+		System.out.println("In Given again");
+	    
+	}
+
+	@When("^verify total page links are (\\d+)$")
+	public void verify_total_page_links_are(int arg1) throws Throwable {
+	  
+		System.out.println("In When");
+	}
+
+	@When("^close browser$")
+	public void close_browser() throws Throwable {
+	    
+		this.closeApplication(true);
+		System.out.println("In When again...");
+	}
 }
---
+++ src/test/java/testcases/home.feature
@@ -0,0 +1,22 @@
+@tag
+Feature: To verify functionality of home page
+Background: 
+	Given User has launched "chrome" browser with url "https://the-internet.herokuapp.com/"
+    And verify page by "title" with "The internet"
+    
+  @tag1
+  Scenario: To verify total link count on home page    
+    When verify total page links are 39
+    And close browser
+   
+   @tag2 
+    Scenario: To verify total link count on home page
+    When verify total page links are 39
+    And close browser
+    
+    @tag3
+    Scenario: To verify total link count on home page
+    When verify total page links are 39
+    And close browser
+    
+  
